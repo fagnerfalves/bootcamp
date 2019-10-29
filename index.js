6ec021473b586d3e59b442e3ed2540060a4bb749
Fagner Alves <fagner.falves@gmail.com>
2019-10-29 16:42:27 -0300

    CHORE - Add the routes

--- index.js
+++ index.js
@@ -2,10 +2,40 @@ const express = import('express');
 
 const app = express();
 
-app.get('/teste', (req,res)=>{
+// route to list all projects
+app.get('/project', (req,res)=>{
   res.json({
     message: "Hello World"
   })
 });
+
+//route to insert a project
+app.post('/project', (req,res)=>{
+  res.json({
+    message: "Hello World"
+  })
+});
+
+// route to update a project
+app.put('/project/:id', (req,res)=>{
+  res.json({
+    message: "Hello World"
+  })
+});
+
+//route to delete a project
+app.delete('/project/:id', (req,res)=>{
+  res.json({
+    message: "Hello World"
+  })
+});
+
+// route to add a task in any project
+app.post('/project/:id/tasks', (req,res)=>{
+  res.json({
+    message: "Hello World"
+  })
+});
+
 
 app.listen(3333);
